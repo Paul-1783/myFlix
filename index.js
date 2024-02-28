d08260b3d69e3be49cffd77b5fc4eb5b1ea734a7
Paul-1783 <paulschmitt144@gmail.com>
2024-02-28 22:24:43 +0100

    Updated CORS 4

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ const cors = require("cors");
 // allowed urls for Cross - Origin Resource Sharing
 let allowedOrigins = [
   "*",
+  "https://paul-1783.github.io",
   "https://paul-1783.github.io/",
   "https://paul-1783.github.io/myFlix-Angular-client",
   "https://paul-1783.github.io/myFlix-Angular-client/welcome",
