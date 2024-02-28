4d0a52fb5d381b73423a9e6699159f352dd41524
Paul-1783 <paulschmitt144@gmail.com>
2024-02-28 18:57:53 +0100

    Updated CORS

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ const cors = require("cors");
 // allowed urls for Cross - Origin Resource Sharing
 let allowedOrigins = [
   "*",
+  "https://paul-1783.github.io/myFlix-Angular-client/welcome",
   "https://myflixmovielibrary.netlify.app",
   "http://localhost:8080",
   "https://myflicsdb3.onrender.com",
